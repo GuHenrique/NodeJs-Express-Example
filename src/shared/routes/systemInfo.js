6c894f42456e0684373bcd872eaf01ae8f3fc750
Gustavo Henrique <gustavo._henrique@hotmail.com>
2020-09-08 17:11:52 -0300

    Status variables update

--- src/shared/routes/systemInfo.js
+++ src/shared/routes/systemInfo.js
@@ -9,13 +9,11 @@ routes.get('/status', async (req, res) => {
 
     var status = {};
 
-    status.name = process.argv[2];
+    status.serverName = process.argv[2];
+    status.date = formattedDateTime(new Date());
     status.aliveAtms = process.uptime();
     status.aliveAt = aliveAtBySeconds(status.aliveAtms)
-    status.date = formattedDateTime(new Date());
-    status.nodeSourceUrl = process.release.sourceUrl;
-    status.nodeCodename = process.release.lts;
-    status.nodeVersion = process.versions.node;
+    status.nodeVersion = `${process.versions.node} - ${process.release.lts}`;
 
     return res.json(status);
 });
